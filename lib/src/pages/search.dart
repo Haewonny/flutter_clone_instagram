a5d1d75b3ba6bbc1c8fa9306ff8cc3ae9c830806
Lee Haewon <hawon2205@ewhain.net>
2024-01-02 17:37:36 +0900

    [4강] 검색 UI - 크기가 다른 이미지 처리

---
+++ lib/src/pages/search.dart
@@ -0,0 +1,108 @@
+import 'dart:math';
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:quiver/iterables.dart';
+
+class Search extends StatefulWidget {
+  const Search({super.key});
+
+  @override
+  State<Search> createState() => _SearchState();
+}
+
+class _SearchState extends State<Search> {
+  List<List<int>> groupBox = [[], [], []];
+  List<int> groupIndex = [0, 0, 0];
+
+  @override
+  void initState() {
+    super.initState();
+    for (var i = 0; i < 100; i++) {
+      var gi = groupIndex.indexOf(min<int>(groupIndex)!); // 몇번째 column 인지
+      var size = 1;
+
+      if (gi != 1) {
+        size = Random().nextInt(100) % 2 == 0 ? 1 : 2;
+      }
+      groupBox[gi].add(size);
+      groupIndex[gi] += size;
+    }
+  }
+
+  Widget _appbar() {
+    return Row(
+      children: [
+        Expanded(
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+            margin: const EdgeInsets.only(left: 15),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(6),
+              color: const Color(0xffefefef),
+            ),
+            child: Row(
+              children: [
+                Icon(Icons.search),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 5),
+                  child: Text(
+                    '검색',
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Color(0xff838383),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Icon(Icons.location_pin),
+        ),
+      ],
+    );
+  }
+
+  Widget _body() {
+    return SingleChildScrollView(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: List.generate(
+          groupBox.length,
+          (index) => Expanded(
+            child: Column(
+                children: List.generate(
+              groupBox[index].length,
+              (jndex) => Container(
+                height: Get.width * 0.33 * groupBox[index][jndex],
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.white),
+                    color: Colors
+                        .primaries[Random().nextInt(Colors.primaries.length)]),
+                child: CachedNetworkImage(imageUrl: 'https://mblogthumb-phinf.pstatic.net/MjAyMjAyMDdfMjEy/MDAxNjQ0MTk0Mzk2MzY3.WAeeVCu2V3vqEz_98aWMOjK2RUKI_yHYbuZxrokf-0Ug.sV3LNWlROCJTkeS14PMu2UBl5zTkwK70aKX8B1w2oKQg.JPEG.41minit/1643900851960.jpg?type=w800', fit: BoxFit.cover,),
+              ),
+            ).toList()),
+          ),
+        ).toList(),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            _appbar(),
+            Expanded(child: _body()),
+          ],
+        ),
+      ),
+    );
+  }
+}
