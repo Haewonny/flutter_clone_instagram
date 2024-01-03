9651bd755ba8cd363c4b79c03354ac6347d2dcb0
Lee Haewon <hawon2205@ewhain.net>
2024-01-03 18:55:11 +0900

    [6강] 업로드 화면 구성

--- lib/src/pages/upload.dart
+++ lib/src/pages/upload.dart
@@ -1,12 +1,136 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_clone_instagram/src/components/image_data.dart';
+import 'package:get/get.dart';
 
 class Upload extends StatelessWidget {
   const Upload({super.key});
 
+  Widget _imagePreview() {
+    return Container(
+      width: Get.width,
+      height: Get.width,
+      color: Colors.grey,
+    );
+  }
+
+  Widget _header() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween, // 갤러리랑 여러 항목 선택 떨어지게
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Row(
+              children: [
+                Text(
+                  '갤러리',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                  ),
+                ),
+                Icon(Icons.arrow_drop_down),
+              ],
+            ),
+          ),
+          Row(
+            children: [
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                decoration: BoxDecoration(
+                    color: const Color(0xff808080),
+                    borderRadius: BorderRadius.circular(30)),
+                child: Row(
+                  children: [
+                    ImageData(IconsPath.imageSelectIcon),
+                    const SizedBox(
+                      width: 7,
+                    ),
+                    const Text(
+                      '여러 항목 선택',
+                      style: TextStyle(color: Colors.white, fontSize: 14),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(
+                width: 5,
+              ),
+              Container(
+                padding: const EdgeInsets.all(6),
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color(0xff808080),
+                ),
+                child: ImageData(
+                  IconsPath.cameraIcon,
+                  width: 45,
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _imageSelectList() {
+    return GridView.builder(
+      physics: const NeverScrollableScrollPhysics(), // 스크롤 하지 않음
+        shrinkWrap: true,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 4,
+          childAspectRatio: 1, // 정사각형
+          mainAxisSpacing: 1,
+          crossAxisSpacing: 1,
+        ),
+        itemCount: 100,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            color: Color(0xff424242),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.lightBlueAccent,
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: GestureDetector(
+          onTap: () {},
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: ImageData(IconsPath.closeImage),
+          ),
+        ),
+        title: const Text(
+          'New Post',
+          style: TextStyle(
+              fontWeight: FontWeight.bold, fontSize: 20, color: Colors.black),
+        ),
+        actions: [
+          GestureDetector(
+            onTap: () {},
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: ImageData(
+                IconsPath.nextImage,
+                width: 50,
+              ),
+            ),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [_imagePreview(), _header(), _imageSelectList()],
+        ),
+      ),
     );
   }
 }
