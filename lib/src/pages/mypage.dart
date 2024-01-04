c01808cfd52699ab7d0dfc62be3e08983513a029
Lee Haewon <hawon2205@ewhain.net>
2024-01-04 18:55:32 +0900

    [9강] 마이페이지 UI

---
+++ lib/src/pages/mypage.dart
@@ -0,0 +1,260 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_clone_instagram/src/components/avatar_widget.dart';
+import 'package:flutter_clone_instagram/src/components/image_data.dart';
+import 'package:flutter_clone_instagram/src/components/user_card.dart';
+
+class MyPage extends StatefulWidget {
+  const MyPage({super.key});
+
+  @override
+  State<MyPage> createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> with TickerProviderStateMixin {
+  late TabController tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(length: 2, vsync: this);
+  }
+
+  Widget _statisticsOne(String title, int value) {
+    return Column(
+      children: [
+        Text(
+          value.toString(),
+          style: const TextStyle(
+            fontSize: 20,
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 4),
+        Text(
+          title,
+          style: const TextStyle(fontSize: 15, color: Colors.black54),
+        ),
+      ],
+    );
+  }
+
+  Widget _information() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Row(
+            children: [
+              AvatarWidget(
+                type: AvatarType.TYPE3,
+                thumbPath: 'https://picpac.kr/common/img/default_profile.png',
+                size: 80,
+              ),
+              const SizedBox(width: 10),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Expanded(child: _statisticsOne('Posts', 15)),
+                    Expanded(child: _statisticsOne('Followers', 180)),
+                    Expanded(child: _statisticsOne('Following', 180)),
+                  ],
+                ),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Text(
+            '해원',
+            style: TextStyle(
+                fontSize: 13, fontWeight: FontWeight.bold, color: Colors.black),
+          ),
+          const SizedBox(
+            height: 3,
+          ),
+          const Text(
+            '@ewha.w.univ',
+            style: TextStyle(
+                fontSize: 13,
+                fontWeight: FontWeight.bold,
+                color: Color(0xff013870)),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _menu() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 25),
+      child: Row(
+        children: [
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 7),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(3),
+                border: Border.all(
+                  color: const Color(0xffdedede),
+                ),
+              ),
+              child: const Text(
+                'Edit profile',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          const SizedBox(width: 8),
+          Container(
+            padding: const EdgeInsets.all(4),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(3),
+              border: Border.all(
+                color: const Color(0xffdedede),
+              ),
+              color: const Color(0xffefefef),
+            ),
+            child: ImageData(IconsPath.addFriend),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _discoverPeople() {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              Text(
+                'Discover People',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                  color: Colors.black,
+                ),
+              ),
+              Text(
+                'See All',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Color(0xff0195f7),
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+        ),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Row(
+              children: List.generate(
+                  10,
+                  (index) => UserCard(
+                      userId: '사람$index',
+                      description: '00$index님이 팔로우합니다.')).toList()),
+        ),
+      ],
+    );
+  }
+
+  Widget _tabMenu() {
+    return TabBar(
+      controller: tabController,
+      indicatorColor: Colors.black,
+      indicatorWeight: 1,
+      tabs: [
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          child: ImageData(IconsPath.gridViewOn),
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          child: ImageData(IconsPath.myTagImageOff),
+        ),
+      ],
+    );
+  }
+
+  Widget _tabView() {
+    return GridView.builder(
+        physics: const NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
+        itemCount: 100,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          childAspectRatio: 1,
+          mainAxisSpacing: 1,
+          crossAxisSpacing: 1,
+        ),
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            color: Colors.grey,
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        title: const Text(
+          'haewonny',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+            color: Colors.black,
+          ),
+        ),
+        actions: [
+          GestureDetector(
+            onTap: () {},
+            child: ImageData(
+              IconsPath.uploadIcon,
+              width: 50,
+            ),
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: ImageData(
+                IconsPath.menuIcon,
+                width: 50,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            _information(),
+            _menu(),
+            _discoverPeople(),
+            const SizedBox(
+              height: 20,
+            ),
+            _tabMenu(),
+            _tabView(),
+          ],
+        ),
+      ),
+    );
+  }
+}
