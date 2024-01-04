4693bed66ac6de5f0be22dbccb182afa4b6de90c
Lee Haewon <hawon2205@ewhain.net>
2024-01-04 17:12:08 +0900

    [8강] 활동 페이지 화면 UI

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_clone_instagram/src/components/image_data.dart';
 import 'package:flutter_clone_instagram/src/controller/bottom_nav_controller.dart';
+import 'package:flutter_clone_instagram/src/pages/active_history.dart';
 import 'package:flutter_clone_instagram/src/pages/home.dart';
 import 'package:flutter_clone_instagram/src/pages/search.dart';
 import 'package:get/get.dart';
@@ -15,73 +16,68 @@ class App extends GetView<BottomNavController> {
     return WillPopScope(
       onWillPop: controller.willPopAction,
       child: Obx(
-            () =>
-            Scaffold(
-              // backgroundColor: Colors.lightBlueAccent,
-              body: IndexedStack(
-                index: controller.pageIndex.value,
-                children: [
-                  const Home(),
-                  Navigator(
-                    key: controller.searchPageNavigationKey,
-                    onGenerateRoute: (routeSetting) {
-                      return MaterialPageRoute(
-                        builder: (context) => const Search(),
-                      );
-                    },
-                  ),
-                  Container(
-                    child: Center(child: Text('UPLOAD')),
-                  ),
-                  Container(
-                    child: Center(child: Text('ACTIVITY')),
-                  ),
-                  Container(
-                    child: Center(child: Text('MYPAGE')),
-                  ),
-                ],
+        () => Scaffold(
+          // backgroundColor: Colors.lightBlueAccent,
+          body: IndexedStack(
+            index: controller.pageIndex.value,
+            children: [
+              const Home(),
+              Navigator(
+                key: controller.searchPageNavigationKey,
+                onGenerateRoute: (routeSetting) {
+                  return MaterialPageRoute(
+                    builder: (context) => const Search(),
+                  );
+                },
               ),
-              bottomNavigationBar: BottomNavigationBar(
-                type: BottomNavigationBarType.fixed,
-                showSelectedLabels: false,
-                showUnselectedLabels: false,
-                currentIndex: controller.pageIndex.value,
-                // 현재 활성화 된 거 컨트롤러로 받아옴
-                elevation: 0,
-                // 음영 지우기
-                onTap: controller.changeBottomNav,
-                items: [
-                  BottomNavigationBarItem(
-                    icon: ImageData(IconsPath.homeOff),
-                    activeIcon: ImageData(IconsPath.homeOn),
-                    label: 'home',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: ImageData(IconsPath.searchOff),
-                    activeIcon: ImageData(IconsPath.searchOn),
-                    label: 'home',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: ImageData(IconsPath.uploadIcon),
-                    label: 'home',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: ImageData(IconsPath.activeOff),
-                    activeIcon: ImageData(IconsPath.activeOn),
-                    label: 'home',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Container(
-                      width: 30,
-                      height: 30,
-                      decoration: const BoxDecoration(
-                          shape: BoxShape.circle, color: Colors.grey),
-                    ),
-                    label: 'home',
-                  ),
-                ],
+              Container(),
+              const ActiveHistory(),
+              Container(
+                child: Center(child: Text('MYPAGE')),
               ),
-            ),
+            ],
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            currentIndex: controller.pageIndex.value,
+            // 현재 활성화 된 거 컨트롤러로 받아옴
+            elevation: 0,
+            // 음영 지우기
+            onTap: controller.changeBottomNav,
+            items: [
+              BottomNavigationBarItem(
+                icon: ImageData(IconsPath.homeOff),
+                activeIcon: ImageData(IconsPath.homeOn),
+                label: 'home',
+              ),
+              BottomNavigationBarItem(
+                icon: ImageData(IconsPath.searchOff),
+                activeIcon: ImageData(IconsPath.searchOn),
+                label: 'home',
+              ),
+              BottomNavigationBarItem(
+                icon: ImageData(IconsPath.uploadIcon),
+                label: 'home',
+              ),
+              BottomNavigationBarItem(
+                icon: ImageData(IconsPath.activeOff),
+                activeIcon: ImageData(IconsPath.activeOn),
+                label: 'home',
+              ),
+              BottomNavigationBarItem(
+                icon: Container(
+                  width: 30,
+                  height: 30,
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.grey),
+                ),
+                label: 'home',
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/src/pages/upload.dart
+++ lib/src/pages/upload.dart
@@ -2,6 +2,7 @@ import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_clone_instagram/src/components/image_data.dart';
+import 'package:get/get.dart';
 import 'package:photo_manager/photo_manager.dart';
 import 'package:typed_data/typed_data.dart';
 
@@ -236,7 +237,7 @@ class _UploadState extends State<Upload> {
         backgroundColor: Colors.white,
         elevation: 0,
         leading: GestureDetector(
-          onTap: () {},
+          onTap: Get.back,
           child: Padding(
             padding: const EdgeInsets.all(15.0),
             child: ImageData(IconsPath.closeImage),
